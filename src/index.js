efd20af9f11e599f10297c1177160fbf0e4cae88
MarinaMuntian <dp060491smv@gmail.com>
2021-07-29 20:38:26 +0300

    add fetchImages.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './sass/main.scss';
 import Notiflix from 'notiflix';
+import GetImagesAPI from './js/fetchImages';
---
+++ src/js/fetchImages.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export default class GetImagesAPI {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.itemsPerPage = 40;
+    }
+
+    fetchImages() {
+        const BASE_URL = 'https://pixabay.com/api/';
+        const API_KEY = '22698897-2ba121c7da348ad857b7f6d23';
+
+        const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.itemsPerPage}`;
+
+        return axios
+            .get(url)
+            .then(response => {
+                this.updatePage();
+                console.log(response.data.totalHits);
+                return response.data;
+            })
+            .catch(error => console.log(error));
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    updatePage() {
+        this.page += 1;
+    }
+}
