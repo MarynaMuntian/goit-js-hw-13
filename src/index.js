68e2be0582656b72b402cc85754393f2ce4e4239
MarinaMuntian <dp060491smv@gmail.com>
2021-07-30 22:01:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const submitBtn = document.querySelector('button[type=submit]');
 const galleryContainer = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-const images = new GetImagesAPI();
+const imagesApi = new GetImagesAPI();
 
 function hideLoadMoreBtn () {
 loadMoreBtn.classList.remove('visible');
@@ -24,4 +24,65 @@ function showLoadMoreBtn() {
 const renderPage = (img) => {
     const markup = imgTemplate(img.hits);
     galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+const clearPage = () => {
+    galleryContainer.innerHTML = '';
+}
+
+const imagesQuantity = (items) => {
+    if (items > 0) {
+        Notify.success(`Hooray! We found ${items} images.`);
+    }
+}
+
+const searchError = () => {
+    imagesApi.resetPage();
+    Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+    );
+    clearPage();
+    hideLoadMoreBtn();
+}
+
+const endError = () => {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+ }
+
+const submitSearch = (event) => {
+    event.preventDefault();
+    clearPage();
+    // hideLoadMoreBtn();
+
+    const input = event.currentTarget;
+    imagesApi.query = input.elements.searchQuery.value.trim();
+    imagesApi.resetPage();
+
+    if (imagesApi.searchQuery !== '') {
+        const request = async () => {
+            try {
+                const images = await imagesApi.fetchImages();
+                imagesQuantity(images.totalHits);
+                if (images.totalHits < 1) {
+                    searchError();
+                    return;
+                } else if (images.totalHits <= imagesApi.itemsPerPage) {
+                    endError();
+                    hideLoadMoreBtn();
+                    renderPage(images);
+                    return;
+                }
+                showLoadMoreBtn();
+                return renderPage(images);
+            } catch (error) {
+                searchError(error);
+            } finally {
+                input.reset();
+            }
+        };
+        return request();
+    } else {
+        clearPage();
+        searchError();
+    }
 }
