2938b993a29c446f462df656ab27cb687979477e
MarinaMuntian <dp060491smv@gmail.com>
2021-07-30 15:11:59 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,20 @@ const searchInput = document.querySelector('input[type=text]');
 const submitBtn = document.querySelector('button[type=submit]');
 const galleryContainer = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+
+const images = new GetImagesAPI();
+
+function hideLoadMoreBtn () {
+loadMoreBtn.classList.remove('visible');
+};
+ 
+hideLoadMoreBtn();
+
+function showLoadMoreBtn() {
+    loadMoreBtn.classList.add('visible');
+}
+
+const renderPage = (img) => {
+    const markup = imgTemplate(img.hits);
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
