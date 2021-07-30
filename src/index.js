b36669ac126cebdde7fc808942486ee682da821f
MarinaMuntian <dp060491smv@gmail.com>
2021-07-30 14:05:46 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './sass/main.scss';
 import Notiflix from 'notiflix';
 import GetImagesAPI from './js/fetchImages';
+import imgTemplate from './templates/img.hbs';
+
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('input[type=text]');
+const submitBtn = document.querySelector('button[type=submit]');
+const galleryContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
